7c4e398f148bb9dc8e0b0b0e15de2c6cfa08311f
p8520888 <p8520888@gmail.com>
2018-03-23 12:19:38 +0800

    add

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            testc;
+            Console.WriteLine("1104105336");
+            Console.ReadKey();
         }
     }
 }
