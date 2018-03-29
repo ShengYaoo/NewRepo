202f2ada0cfad64b0520ccd31ee06986d2c2bd61
Kiyoshimo <jason.asuka@gmail.com>
2018-03-29 23:44:34 +0800

    AE86
    也许是这样？

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Your Name");
+	    Console.WriteLine("我是驱逐舰清霜！I am Destroyer,Kiyoshimo！");
         }
     }
 }
