32a9ec1e2afd9ddd7970c33d6b01c17f28533707
Kiyoshimo <jason.asuka@gmail.com>
2018-03-29 23:49:28 +0800

    AE87
    大概是这样？

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your Name");
+	    Console.WriteLine("我是驱逐舰清霜！I am Destroyer,Kiyoshimo！");
+        }
+    }
+}
